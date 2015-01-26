5446fd2480ff6604f23e1aa2aef070007300ee41
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2015-01-26 18:17:22 +0000

    remove env restrictions from sudoers

--- sbin/config.ubuntu.sh
+++ sbin/config.ubuntu.sh
@@ -5,7 +5,7 @@
 
 print_h2 "Configure system"
 
-sudo $SHELL_COMMONS_HOME_DIR/bin/file_remove_str "Defaults \+env_reset" /etc/sudoers
-sudo $SHELL_COMMONS_HOME_DIR/bin/file_remove_str "Defaults \+secure_path=\".*\"" /etc/sudoers
+sudo $SHELL_COMMONS_HOME_DIR/bin/file_remove_str "Defaults[[:space:]]+env_reset" /etc/sudoers
+sudo $SHELL_COMMONS_HOME_DIR/bin/file_remove_str "Defaults[[:space:]]+secure_path=\".*\"" /etc/sudoers
 
 exit 0
