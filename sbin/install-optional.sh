a03570ab8a236c3158b65e51d500623164724f30
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2015-03-08 23:46:25 +0000

    install additional npm packages

--- sbin/install-optional.sh
+++ sbin/install-optional.sh
@@ -20,8 +20,9 @@ sudo npm install -g grunt-cli
 sudo npm install -g gulp
 sudo npm install -g bower
 sudo npm install -g yo
-sudo npm install -g generator-generator
 sudo npm install -g cordova
+sudo npm install -g generator-generator
+sudo npm install -g generator-jhipster
 
 ################################################################################
 
