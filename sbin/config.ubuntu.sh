e4d3df27b48e3ad6e9259f575c34a2efa0416f07
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2015-05-20 17:15:20 +0100

    update

--- sbin/config.ubuntu.sh
+++ sbin/config.ubuntu.sh
@@ -3,6 +3,7 @@
 ################################################################################
 # locale
 
+print_h2 "Configure locale"
 sudo locale-gen en_GB.UTF-8
 sudo dpkg-reconfigure locales
 
