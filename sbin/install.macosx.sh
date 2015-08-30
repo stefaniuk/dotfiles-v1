2845b3b09dc46921748415c2aa0de65a3ae5003a
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2015-08-30 09:10:52 +0100

    Mac OSX install more software

--- sbin/install.macosx.sh
+++ sbin/install.macosx.sh
@@ -70,28 +70,45 @@ fi
 
 if [ -n "$arg_install_workstation_tools" ] && [ -z "$arg_install_server_tools" ]; then
     print_h2 "Install workstation tools"
+
     brew install git-flow
     brew install gtypist
     brew install irssi
     brew install zsh
-    brew cask install boxcryptor
-    brew cask install dockertoolbox
-    brew cask install dropbox
-    brew cask install filezilla
-    brew cask install firefox
-    brew cask install firefoxdeveloperedition
-    brew cask install github-desktop
-    brew cask install google-chrome
-    brew cask install google-drive
-    brew cask install iterm2-beta
     brew cask install java
-    brew cask install megasync
-    brew cask install phpstorm
+    # configuration and monitoring tools
+    brew cask install seil
+    brew cask install karabiner
+    brew cask install bettertouchtool
+    brew cask install witch
+    brew cask install moom
+    brew cask install asepsis
+    brew cask install caffeine
+    brew cask install menumeters
+    brew cask install android-file-transfer
+    # developer tools
+    brew cask install iterm2-beta
+    brew cask install sublime-text3
+    brew cask install virtualbox
+    brew cask install vagrant
+    brew cask install dockertoolbox
+    brew cask install github-desktop
     brew cask install smartgit
     brew cask install smartsynchronize
-    brew cask install sublime-text3
-    brew cask install vagrant
-    brew cask install virtualbox
+    brew cask install filezilla
+    brew cask install phpstorm
+    # applications and utils
+    brew cask install xtrafinder
+    brew cask install google-chrome
+    brew cask install firefox
+    brew cask install firefoxdeveloperedition
+    brew cask install dropbox
+    brew cask install megasync
+    brew cask install google-drive
+    brew cask install boxcryptor
+    brew cask install xee
+    brew cask install vox
+    brew cask install vlc
 fi
 
 ################################################################################
