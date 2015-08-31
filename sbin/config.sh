33a6ce3de08e30b23d7318eea1be4ac45404ecc1
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2015-08-31 14:33:50 +0100

    Configure Bash

--- sbin/config.sh
+++ sbin/config.sh
@@ -18,15 +18,13 @@ fi
 # Bash
 
 if which bash > /dev/null 2>&1; then
-
     print_h2 "Configure Bash"
-
     # resources
     cp -f ~/etc/bash/.bash* ~
     file_replace_str "USER_NAME=\"unknown\"" "USER_NAME=\"$USER_NAME\"" ~/.bash_exports
     file_replace_str "USER_EMAIL=\"unknown\"" "USER_EMAIL=\"$USER_EMAIL\"" ~/.bash_exports
     file_replace_str "GITHUB_ACCOUNT=\"unknown\"" "GITHUB_ACCOUNT=\"$GITHUB_ACCOUNT\"" ~/.bash_exports
-
+    file_replace_str "GITLAB_ACCOUNT=\"unknown\"" "GITLAB_ACCOUNT=\"$GITLAB_ACCOUNT\"" ~/.bash_exports
     # profile
     [ -f ~/.profile ] && [ ! -f ~/.profile.old ] && mv ~/.profile ~/.profile.old
     [ -f ~/.bash_profile ] && [ ! -f ~/.bash_profile.old ] && mv ~/.bash_profile ~/.bash_profile.old
