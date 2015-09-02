8dcd8273a7229cf175b9a23c5166a273dfe92e5b
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2015-09-02 13:47:49 +0100

    Do not configure Atom

--- sbin/config.ubuntu.sh
+++ sbin/config.ubuntu.sh
@@ -19,28 +19,5 @@ if which conky > /dev/null 2>&1; then
 fi
 
 ################################################################################
-# Atom
-
-if which atom > /dev/null 2>&1; then
-
-    print_h2 "Configure Atom"
-
-    # resources
-    mkdir -p ~/.atom
-    cp -f ~/etc/atom/* ~/.atom
-
-    # packages
-    #apm install compare-files
-    apm install git-history
-    apm install git-log
-    apm install git-plus
-    apm install language-ini
-    apm install open-last-project
-    apm install open-recent
-    apm install sort-lines
-
-fi
-
-################################################################################
 
 exit 0
