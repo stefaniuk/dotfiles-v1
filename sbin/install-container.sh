c50e0b332f93b0cd05f8687c0464a48291e93c74
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2015-11-16 06:54:59 +0000

    Do not install anything if this is a container

--- sbin/install-container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-system_info
-
-exit 0
--- sbin/install.sh
+++ sbin/install.sh
@@ -5,11 +5,11 @@ print_h1 "Installing..."
 ################################################################################
 # install distribution specific tools
 
-if [ -z "$VIRTUALISATION" ] && [ -f ~/sbin/install-$DIST.sh ]; then
-    (. ~/sbin/install-$DIST.sh $*)
-elif [ "$VIRTUALISATION" == "container" ] && [ -f ~/sbin/install-container.sh ]; then
-    (. ~/sbin/install-container.sh $*)
+file=~/sbin/install-$DIST.sh
+if [ -f $file ] && [ -z "$VIRTUALISATION" ]; then
+    (. $file $*)
 fi
+unset file
 
 ################################################################################
 
