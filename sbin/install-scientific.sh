2664ccfc81f86172dd1ccd045806b6038219f13c
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2015-11-21 21:56:40 +0000

    Work on Scientific Linux done

--- sbin/install-scientific.sh
+++ sbin/install-scientific.sh
@@ -11,20 +11,55 @@ fi
 
 print_h2 "Install dotfiles tools"
 $yum_install \
+    bash-completion \
+    bc \
+    curl \
+    expect \
+    openssl \
+    pcre-tools \
+    perl \
+    python python34 \
+    rsync \
+    sudo \
+    unzip \
+    wget \
+    xz
+    # PROBLEMS: pcre-tools python34
 
 if [ -n "$arg_install_common_tools" ]; then
     print_h2 "Install common tools"
     $yum_install \
+        ack \
+        grc \
+        htop \
+        lnav \
+        mc \
+        patch \
+        strace \
+        traceroute \
+        tree \
+        vim \
+        zsh
+        # PROBLEMS: grc
 fi
 
 if [ -n "$arg_install_server_tools" ]; then
     print_h2 "Install server tools"
     $yum_install \
+        mailx
 fi
 
 if [ -n "$arg_install_workstation_tools" ]; then
     print_h2 "Install workstation tools"
     $yum_install \
+        git \
+        gitflow \
+        gtypist \
+        irssi \
+        java-1.7.0-openjdk \
+        lynx \
+        tmux
+        # PROBLEMS: gtypist
 fi
 
 ################################################################################
