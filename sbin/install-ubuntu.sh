56ae0aea937fd46d688f3e951e779083c8e2fbf1
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2015-11-22 12:18:22 +0000

    Working on Ubuntu

--- sbin/install-ubuntu.sh
+++ sbin/install-ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+apt_get_update="sudo apt-get --yes update"
 apt_get_install="sudo apt-get --yes --ignore-missing --no-install-recommends install"
+apt_add_repository="sudo add-apt-repository --yes"
 DEBIAN_FRONTEND="noninteractive"
 
 ################################################################################
@@ -64,18 +66,46 @@ if [ -n "$arg_install_workstation_tools" ]; then
         openjdk-7-jdk \
         tmux
     # compiz
-    #$apt_get_install \
-    #    compiz \
-    #    compizconfig-settings-manager \
-    #    compiz-plugins-default \
-    #    compiz-plugins
+    $apt_get_install \
+        compiz \
+        compizconfig-settings-manager \
+        compiz-plugins-default \
+        compiz-plugins
     # conky
-    #sudo add-apt-repository --yes ppa:teejee2008/ppa
-    #sudo apt-get update
-    #$apt_get_install \
-    #    conky conky-manager \
-    #    lm-sensors \
-    #    hddtemp
+    $apt_add_repository ppa:teejee2008/ppa
+    $apt_get_update
+    $apt_get_install \
+        conky \
+        conky-manager \
+        lm-sensors hddtemp
+    # sublime text
+    $apt_add_repository ppa:webupd8team/sublime-text-3
+    $apt_get_update
+    $apt_get_install \
+        sublime-text-installer
+    # virtualbox
+    wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc | sudo apt-key add -
+    sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian $PSEUDO_NAME non-free contrib' > /etc/apt/sources.list.d/virtualbox.list"
+    $apt_get_update
+    $apt_get_install \
+        virtualbox-5.0
+    # vagrant
+    $apt_get_install \
+        vagrant
+    # docker
+    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+    sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-$PSEUDO_NAME main' > /etc/apt/sources.list.d/docker.list"
+    $apt_get_update
+    $apt_get_install \
+        docker-engine
+    $ver=$(www_get https://github.com/docker/compose | grep '/docker/compose/tree/' | egrep '/[0-9]+\.[0-9+]\.[0-9]+"' | egrep -o '[0-9]+\.[0-9+]\.[0-9]+' | sortvr | head -n 1)
+    sudo curl -L https://github.com/docker/compose/releases/download/$ver/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    # chrome
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c "echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list"
+    $apt_get_update
+    $apt_get_install \
+        google-chrome-stable
 fi
 
 ################################################################################
