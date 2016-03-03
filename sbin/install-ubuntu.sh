7945bd5d67942c28b41ad5b64bc954d7c60d5a9f
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2016-03-03 15:47:35 +0000

    Install Ruby and RubyGems

--- sbin/install-ubuntu.sh
+++ sbin/install-ubuntu.sh
@@ -75,34 +75,34 @@ if should_install "workstation"; then
         openjdk-7-jdk \
         tmux
 
-    # compiz
+    # Compiz
     $apt_get_install \
         compiz \
         compizconfig-settings-manager \
         compiz-plugins-default \
         compiz-plugins
-    # conky
+    # Conky
     $apt_add_repository ppa:teejee2008/ppa
     $apt_get_update
     $apt_get_install \
         conky \
         conky-manager \
         lm-sensors hddtemp
-    # sublime text
+    # Sublime Text
     $apt_add_repository ppa:webupd8team/sublime-text-3
     $apt_get_update
     $apt_get_install \
         sublime-text-installer
-    # virtualbox
+    # VirtualBox
     wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc | sudo apt-key add -
     sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian $PSEUDO_NAME non-free contrib' > /etc/apt/sources.list.d/virtualbox.list"
     $apt_get_update
     $apt_get_install \
         virtualbox-5.0
-    # vagrant
+    # Vagrant
     $apt_get_install \
         vagrant
-    # docker
+    # Docker
     sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
     sudo sh -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-$PSEUDO_NAME main' > /etc/apt/sources.list.d/docker.list"
     $apt_get_update
@@ -111,12 +111,17 @@ if should_install "workstation"; then
     ver=$(www_get https://github.com/docker/compose | grep '/docker/compose/tree/' | egrep '/[0-9]+\.[0-9+]\.[0-9]+"' | egrep -o '[0-9]+\.[0-9+]\.[0-9]+' | sortvr | head -n 1)
     sudo curl -L https://github.com/docker/compose/releases/download/$ver/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    # chrome
+    # Chrome
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
     sudo sh -c "echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list"
     $apt_get_update
     $apt_get_install \
         google-chrome-stable
+    # Ruby and RubyGems
+    $apt_get_install \
+        ruby-full
+    gem install \
+        jekyll
 fi
 
 ################################################################################
