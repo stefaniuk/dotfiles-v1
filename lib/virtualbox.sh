bd1180097e7b610fb8a67f9ea00cf1b23bc7af06
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2016-11-09 13:42:26 +0000

    Fix VirtualBox installation

--- lib/virtualbox.sh
+++ lib/virtualbox.sh
@@ -3,8 +3,9 @@
 print_h2 "VirtualBox"
 
 if [ $DIST = "ubuntu" ]; then
-    wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc | sudo apt-key add -
-    sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian $PSEUDO_NAME non-free contrib' > /etc/apt/sources.list.d/virtualbox.list"
+    wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+    sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian $PSEUDO_NAME contrib' > /etc/apt/sources.list.d/virtualbox.list"
     $apt_get_update
     $apt_get_install \
         virtualbox-5.1
