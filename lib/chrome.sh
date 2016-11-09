f49dd82288e3daccb8dd6f91f4d0cf833eee13fc
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2016-11-09 13:42:12 +0000

    Fix Chrome installation

--- lib/chrome.sh
+++ lib/chrome.sh
@@ -3,6 +3,9 @@
 print_h2 "Chrome"
 
 if [ $DIST = "ubuntu" ]; then
+    $apt_get_install \
+        libappindicator1 \
+        libindicator7
     file_download \
         --url "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" \
         --file "google-chrome-stable_current_amd64.deb"
