9b2f41d0753d10f77513ef8b93244f32aa1bca51
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2016-11-15 19:22:45 +0000

    Better defined condition

--- sbin/ubuntu-initialise.sh
+++ sbin/ubuntu-initialise.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if [ -f /etc/sudoers ]; then
+if [ -f /etc/sudoers ] && cat /etc/sudoers | grep "env_reset" > /dev/null || cat /etc/sudoers | grep "secure_path" > /dev/null; then
     print_h2 "Sudo"
-    if cat /etc/sudoers | grep "env_reset"; then
-        sudo $DIR/bin/file_remove_str "Defaults[[:space:]]+env_reset" /etc/sudoers
-    fi
-    if cat /etc/sudoers | grep "secure_path"; then
-        sudo $DIR/bin/file_remove_str "Defaults[[:space:]]+secure_path=\".*\"" /etc/sudoers
-    fi
+    sudo $DIR/bin/file_remove_str "Defaults[[:space:]]+env_reset" /etc/sudoers
+    sudo $DIR/bin/file_remove_str "Defaults[[:space:]]+secure_path=\".*\"" /etc/sudoers
 fi
